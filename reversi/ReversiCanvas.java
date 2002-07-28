20f603dee88d22e1e13c2f4064e7f3e2c1eea344
Andras Salamon <andras.salamon@melda.info>
2002-07-28 10:10:53 +0000

    draw pieces

--- reversi/ReversiCanvas.java
+++ reversi/ReversiCanvas.java
@@ -14,10 +14,14 @@ import javax.microedition.lcdui.*;
 
 public class ReversiCanvas extends Canvas {
     
+    public static final int SIZE = 8;
+    
     public ReversiCanvas(Display display) {
         this.display = display;
         width = getWidth();
         height = getHeight();
+        sizex = width / SIZE;
+        sizey = width / SIZE;
     }
 
     protected void paint(Graphics g) {
@@ -27,11 +31,40 @@ public class ReversiCanvas extends Canvas {
         int h = g.getClipHeight();
 
         // Draw the frame 
-        g.setColor(0xff0000);
+        g.setColor(0xffffff);
         g.fillRect(x, y, w, h);    
+        drawTable(g);
+        drawPiece(g,3,3,0);
+        drawPiece(g,4,4,0);
+        drawPiece(g,3,4,1);
+        drawPiece(g,4,3,1);
+        
+    }
+
+    protected void drawTable(Graphics g) {
+        g.setColor(0x000000);
+        for( int i=0; i<SIZE; ++i ) {
+            g.drawLine(0, i*sizey, width, i*sizey);
+            g.drawLine(i*sizex, 0, i*sizex, height);
+        }
+    }
+
+    protected void drawPiece(Graphics g,int row, int col, int player) {
+        int x = row*sizex+sizex/6;
+        int y = col*sizey+sizey/6;
+        int w = 2*sizex/3;
+        int h = 2*sizey/3;
+        int sa = 0;
+        int aa = 360;
+        if( player == 0 ) {
+            g.drawArc(x,y,w,h,sa,aa);
+        } else {
+            g.fillArc(x,y,w,h,sa,aa);
+        }
     }
 
     Display display;
     int width, height;
+    int sizex, sizey;
     
 } // ReversiCanvas
