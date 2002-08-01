b7872b39ffcef3f91a1322fc8f9665771a36140a
Andras Salamon <andras.salamon@melda.info>
2002-08-01 17:04:36 +0000

    selection with arrow buttons

--- reversi/ReversiCanvas.java
+++ reversi/ReversiCanvas.java
@@ -22,6 +22,8 @@ public class ReversiCanvas extends Canvas {
         height = getHeight();
         sizex = width / SIZE;
         sizey = width / SIZE;
+        selx = 0;
+        sely = 0;
     }
 
     protected void paint(Graphics g) {
@@ -38,7 +40,7 @@ public class ReversiCanvas extends Canvas {
         drawPiece(g,4,4,0);
         drawPiece(g,3,4,1);
         drawPiece(g,4,3,1);
-        
+        drawSelectionBox(g);
     }
 
     protected void drawTable(Graphics g) {
@@ -63,8 +65,36 @@ public class ReversiCanvas extends Canvas {
         }
     }
 
+    protected void drawSelectionBox(Graphics g) {
+        g.setColor(0x008f00);
+        g.drawRect( selx*sizex, sely*sizey,sizex, sizey);
+        g.drawRect( selx*sizex+1, sely*sizey+1,sizex-2, sizey-2);
+    }
+
+    public void keyPressed(int keyCode) {
+        switch( getGameAction(keyCode) ) {
+        case Canvas.UP: 
+            sely = (sely + SIZE -1) % SIZE;
+            repaint();
+            break;
+        case Canvas.DOWN: 
+            sely = (sely + 1) % SIZE;
+            repaint();
+            break;
+        case Canvas.LEFT: 
+            selx = (selx + SIZE -1) % SIZE;
+            repaint();
+            break;
+        case Canvas.RIGHT: 
+            selx = (selx + 1) % SIZE;
+            repaint();
+            break;
+        }
+    }
+
     Display display;
     int width, height;
     int sizex, sizey;
+    int selx, sely;
     
 } // ReversiCanvas
