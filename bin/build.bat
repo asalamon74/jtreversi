ca505600c29ad7937599966282589141536ba716
Andras Salamon <andras.salamon@melda.info>
2002-09-18 19:58:45 +0000

    Windows run and build files added

---
+++ bin/build.bat
@@ -0,0 +1,22 @@
+set JAVA_HOME=e:\Programs\jdk1.3.1_01
+set WTK_HOME=e:\Programs\WTK104
+
+rem echo Creating directories
+rem echo \(This stage may produce already exist errors. Ignore them.\)
+
+mkdir ..\tmpclasses
+mkdir ..\classes
+
+rem echo Compiling source files
+
+%JAVA_HOME%\bin\javac -bootclasspath %WTK_HOME%/lib/midpapi.zip -d ..\tmpclasses -classpath ..\tmpclasses ..\reversi\*.java ..\minimax\*.java
+
+rem echo Preverifying class files
+
+%WTK_HOME%\bin\preverify -classpath %WTK_HOME%\lib\midpapi.zip;..\tmpclasses -d ..\classes ..\tmpclasses
+
+rem echo Jaring preverified class files
+%JAVA_HOME%\bin\jar cmf MANIFEST.MF j2me_reversi.jar -C ..\classes .
+
+rem echo Don\'t forget to update the JAR file size in the JAD file
+
---
+++ bin/run.bat
@@ -0,0 +1,6 @@
+@echo off
+
+set WTK_HOME=e:/Programs/WTK104
+
+%WTK_HOME%/bin/emulator -classpath j2me_reversi.jar -Xdescriptor:j2me_reversi.jad
+
