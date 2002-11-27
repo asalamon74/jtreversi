8bdcb397f841cd8d24aad811cd8086f61fb3bd75
Andras Salamon <andras.salamon@melda.info>
2002-11-27 17:31:53 +0000

    run script modifications

--- bin/run.bat
+++ bin/run.bat
@@ -2,5 +2,5 @@
 
 set WTK_HOME=e:/Programs/WTK104
 
-%WTK_HOME%/bin/emulator -classpath j2me_reversi.jar -Xdescriptor:j2me_reversi.jad
+%WTK_HOME%/bin/emulator -classpath jtReversi.jar -Xdescriptor:jtReversi.jad
 
--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export WTK_HOME=/usr/local/java/WTK104
 
-$WTK_HOME/bin/emulator -Xdescriptor:j2me_reversi.jad -Xdevice:DefaultColorPhone
+$WTK_HOME/bin/emulator -Xdescriptor:jtReversi.jad -Xdevice:DefaultColorPhone
 
